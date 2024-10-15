a8570f505665fee50e6198757e48bd9b991f375c
Leighton Miguel <leim56@hawaii.rr.com>
2024-10-15 12:05:29 -1000

    Safe list dynamic width

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const arbitraryWidthSafeList = () => {
+  let ret = [];
+  for (let i = 0; i < 101; i++) {
+    ret.push(`w-[${i}%]`);
+  }
+  return ret;
+};
+
 module.exports = {
   content: ["./templates/**/*.html"],
-  theme: {
-    extend: {},
-  },
+  safelist: arbitraryWidthSafeList(),
   plugins: [require("flowbite/plugin")],
 };
